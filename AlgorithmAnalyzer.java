a7f010c9dc329eaf448c20622102328ccca30ac1
Pasindu Deshan <dpasindu994@gmail.com>
2025-11-15 13:52:22 +0530

    QUICK SORT

--- AlgorithmAnalyzer.java
+++ AlgorithmAnalyzer.java
@@ -72,3 +72,63 @@ public class AlgorithmAnalyzer {
             System.out.printf("%-11d| %.4f%n", n, timeMs);
         }
     }
+
+
+    // ================= QUICK SORT ================= pasindu
+    static void testQuickSort(int[] sizes) {
+        Random rand = new Random();
+
+        System.out.println("Algorithm: Quick Sort");
+        System.out.println("Input Size | Time (ms)");
+        System.out.println("----------------------");
+
+        for (int n : sizes) {
+            int[] arr = createArray(n, rand);
+
+            long start = System.nanoTime();
+            quickSort(arr, 0, arr.length - 1);
+            long end = System.nanoTime();
+
+            double timeMs = (end - start) / 1e6;
+            System.out.printf("%-11d| %.4f%n", n, timeMs);
+        }
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    // ================= UTILITY =================
+    static int[] createArray(int n, Random rand) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
+        return arr;
+    }
+
+}
