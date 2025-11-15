d0aa0b461b3f08dc0c375bf4b09cda3f6174ec73
Ujali Bakmeewewa <bakmeewewaujali@gmail.com>
2025-11-15 12:00:20 +0530

    Adding Linear search

--- AlgorithmAnalyzer.java
+++ AlgorithmAnalyzer.java
@@ -10,13 +10,44 @@ public class AlgorithmAnalyzer {
         testLinearSearch(sizes);
         System.out.println();
 
-        testBinarySearch(sizes);
-        System.out.println();
 
-        testBubbleSort(sizes);
-        System.out.println();
-
-        testQuickSort(sizes);
-        System.out.println();
     }
+
+            // ================= UTILITY =================
+    static int[] createArray(int n, Random rand) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
+        return arr;
+    }
+
+        // ================= LINEAR SEARCH =================
+    static void testLinearSearch(int[] sizes) {
+        Random rand = new Random();
+
+        System.out.println("Algorithm: Linear Search");
+        System.out.println("Input Size | Time (ms)");
+        System.out.println("----------------------");
+
+        for (int n : sizes) {
+            int[] arr = createArray(n, rand);
+            int key = arr[n - 1];
+
+            long start = System.nanoTime();
+            linearSearch(arr, key);
+            long end = System.nanoTime();
+
+            double timeMs = (end - start) / 1e6;
+            System.out.printf("%-11d| %.4f%n", n, timeMs);
+        }
+    }
+
+    static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) return i;
+        }
+        return -1;
+    }
+
 }
